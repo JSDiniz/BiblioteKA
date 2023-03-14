e63f78088c15da41164a38c9ecca67c3537faf85
raphaeldamasceno <rdamasceno1852@gmail.com>
2023-03-14 12:02:32 -0300

    feat/docmentation feito a doc e resolvido bug de criar a mesma duplicata de livros

--- _biblioteKA/settings.py
+++ _biblioteKA/settings.py
@@ -93,10 +93,6 @@ WSGI_APPLICATION = "_biblioteKA.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "postgres": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USERNAME"),
