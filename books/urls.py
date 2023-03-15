3a10df50d8dfc1e925332c7bdc2107d03160a01a
JunielsonDiniz <juniosantos_@hotmail.com>
2023-03-15 15:29:15 -0300

    feat: atualização envio de email

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("books/", views.BookView.as_view()),
     path("books/<pk>/", views.BookDetailView.as_view()),
-
     path("books/follow/<str:book_id>/", views.FollowView.as_view()),
     path("books/follow/<str:book_id>/user/", views.FollowDetailView.as_view()),
 ]
